c72622662f34fc88c0f25632e8eb1eba7cad3b81
dldnlee <sungeun8877@gmail.com>
2024-11-19 09:41:20 +0900

    [Fix] - Unused imports removed

--- app/orderinfo/list/[id]/page.tsx
+++ app/orderinfo/list/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useParams } from "next/navigation";
 import { FiArrowLeft } from "react-icons/fi";
 
 const dummyMedicines = [
